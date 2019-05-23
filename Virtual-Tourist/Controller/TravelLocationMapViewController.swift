5b8a66f6b66b91ebbd0b6f1c1037116a9fdc7075
John Berndt <johnwberndt@gmail.com>
2019-05-22 21:50:02 -0400

    (1) Arranged files into Model-View-Controller groups (2) Navigation Controller is in place with TravelLocationsMapView being the initial view

--- Virtual-Tourist/Controller/TravelLocationMapViewController.swift
+++ Virtual-Tourist/Controller/TravelLocationMapViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TravelLocationMapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
